38a42ce31253e4019cd946eb17c0b332470ece5c
diki45 <pershinikita2002@gmail.com>
2024-01-09 21:29:09 +0500

    remove duplicate "main"

--- Yandex/Yandex.cpp
+++ Yandex/Yandex.cpp
@@ -1,9 +1,4 @@
-﻿// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
-// 271
-// Закомитьте изменения и отправьте их в свой репозиторий.
-
-#include <algorithm>
+﻿#include <algorithm>
 #include <iostream>
 #include <map>
 #include <math.h>
@@ -91,8 +86,8 @@ public:
 
     // находим топ документов по релевантности (по запросу)  
     vector<Document> FindTopDocuments(const string& raw_query) const {
-        Query query_words = ParseQuery(raw_query);         // разделение на + и - слова
-        auto matched_documents = FindAllDocuments(query_words);  // находим релевантность документов по запросу
+        Query query_words = ParseQuery(raw_query);
+        auto matched_documents = FindAllDocuments(query_words);
 
         // сортирую по релевантности
         sort(matched_documents.begin(), matched_documents.end(),
@@ -111,7 +106,7 @@ private:
     // кличество документов
     int number_documents_ = 0;
 
-
+    // int -> id, double -> TF слова
     map<string, map<int, double>> word_to_document_freqs_;
 
 
@@ -153,15 +148,13 @@ private:
 
 
 
-    double СountIDF()
+    double СountIDF(string plus_word) const {
+        return log(double(number_documents_) / word_to_document_freqs_.at(plus_word).size());
+    }
 
 
 
-
-
-
-
-        vector<Document> FindAllDocuments(const Query& query_words) const {
+    vector<Document> FindAllDocuments(const Query& query_words) const {
 
 
         vector<Document> documents;             // для возращения всех id документов и их релевантности
@@ -170,13 +163,11 @@ private:
 
 
         for (const auto& plus_word : query_words.plus_words) {
-            if (word_to_document_freqs_.count(i) == true) {                          //если +слово найдено среди слов документа тогда считаем окончательную релевантость
+            if (word_to_document_freqs_.count(plus_word) == true) {                          //если +слово найдено среди слов документа тогда считаем окончательную релевантость
                 for (const auto& pair_word : word_to_document_freqs_.at(plus_word)) {
 
-                    // IDF -> количество всего документов / на количесво встречаемых в документах
-                    double IDF = log(double(number_documents_) / word_to_document_freqs_.at(plus_word).size());
-
-                    document_to_relevance[pair_word.first] += (IDF * pair_word.second);       // переумножение IDF и TF
+                    ;
+                    document_to_relevance[pair_word.first] += (СountIDF(plus_word) * pair_word.second);
                 }
             }
         }
@@ -184,8 +175,8 @@ private:
         // удаление контейнеров с минус словами
         for (const auto& minus_word : query_words.minus_words) {
             if (word_to_document_freqs_.count(minus_word) == true) {
-                for (const auto& j : word_to_document_freqs_.at(minus_word)) {
-                    document_to_relevance.erase(j.first);
+                for (const auto& pair_word : word_to_document_freqs_.at(minus_word)) {
+                    document_to_relevance.erase(pair_word.first);
                 }
             }
         }
@@ -206,29 +197,32 @@ private:
 
 
 
- 
+
 
 SearchServer CreateSearchServer() {
-    SearchServer search_server;  
-    search_server.SetStopWords(ReadLine());            // ввод стоп слов
+    SearchServer search_server;
+    search_server.SetStopWords(ReadLine());
 
-    const int document_count = ReadLineWithNumber();        // считывание количество документов
+    const int document_count = ReadLineWithNumber();
     // запись всех документов
-    for (int document_id = 0; document_id < document_count; ++document_id) { 
-        search_server.AddDocument(document_id, ReadLine());        // записываем TF каждого слово 
+    for (int document_id = 0; document_id < document_count; ++document_id) {
+        search_server.AddDocument(document_id, ReadLine());
     }
 
     return search_server;
 }
 
-int main() { 
+int main() {
 
-    // добавить const
-    const SearchServer search_server = CreateSearchServer(); // записываем документы и слова
+    const SearchServer search_server = CreateSearchServer();
 
-    const string query = ReadLine(); // записываем слова по которым нужно найти 
+    const string query = ReadLine();
     for (const auto& i : search_server.FindTopDocuments(query)) {
         cout << "{ document_id = "s << i.id << ", "
             << "relevance = "s << i.relevance << " }"s << endl;
     }
 }
+
+
+
+
